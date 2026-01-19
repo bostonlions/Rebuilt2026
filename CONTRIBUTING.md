ba71a4c9d818b5cfa46041a52214f66432817d0c
frc <82274010+andrew-a-johnson@users.noreply.github.com>
2026-01-19 18:23:37 -0500

    Init. Comit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,8 @@
+# Useful links for writing code for this project:
+- [java api](https://docs.oracle.com/javase/8/docs/api/overview-summary.html)
+- wpilib:
+  - [Higher-level docs](https://docs.wpilib.org/en/stable/)
+  - [Base API](https://github.wpilib.org/allwpilib/docs/release/java/index.html)
+- CTRE (motors and sensors):
+  - [Homepage](https://store.ctr-electronics.com/)
+  - [API](https://api.ctr-electronics.com/phoenix6/release/java/index.html)
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.*;
+import static frc.robot.ControlBoard.CustomXboxController.Button;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import frc.robot.subsystems.Drivetrain;
+
+public final class RobotContainer {
+    private final Telemetry logger = new Telemetry(Constants.SwerveConstants.kSpeedAt12Volts.in(MetersPerSecond));
+    private final ControlBoard controller = ControlBoard.getInstance();
+    private final Drivetrain drivetrain = Drivetrain.getInstance();
+
+    public RobotContainer() {
+        drivetrain.setDefaultCommand( // X is defined as forward according to WPILib convention, and Y is defined as to the left
+            drivetrain.applyRequest(() -> // ...but our controller convention is y as forward, and x as to the right
+                Constants.SwerveConstants.drive.withVelocityX(controller.getSwerveTranslation().getY())
+                    .withVelocityY(-controller.getSwerveTranslation().getX())
+                    .withRotationalRate(-controller.getSwerveRotation())
+            )
+        );
+
+        RobotModeTriggers.disabled().whileTrue(
+            drivetrain.applyRequest(() -> Constants.SwerveConstants.idle).ignoringDisable(true)
+        );
+
+        new Trigger(() -> controller.operator.getButton(Button.A)).onTrue(
+            new InstantCommand(drivetrain::zeroGyro).ignoringDisable(true)
+        );
+
+        drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    public Command getAutonomousCommand() {
+        // Simple drive forward auton:
+        return Commands.sequence(
+            // Reset our field centric heading to match the robot
+            // facing away from our alliance station wall (0 deg).
+            drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
+            // Then slowly drive forward (away from us) for 5 seconds.
+            drivetrain.applyRequest(() ->
+                Constants.SwerveConstants.drive.withVelocityX(0.5)
+                    .withVelocityY(0)
+                    .withRotationalRate(0)
+            )
+            .withTimeout(5.0),
+            // Finally idle for the rest of auton
+            drivetrain.applyRequest(() -> Constants.SwerveConstants.idle)
+        );
+    }
+}
